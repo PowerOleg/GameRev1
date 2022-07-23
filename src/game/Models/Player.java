ac9e188639a44a5fe0614ae6eeb7f81d0638a16e
oleg <geraskin.oleg@mail.ru>
2022-07-23 23:36:01 +0500

    A player rev1 was created

---
+++ src/game/Models/Player.java
@@ -0,0 +1,23 @@
+package game.Models;
+
+public class Player {
+    private final String NAME;
+    private final String FIGURE;
+
+    public Player(String NAME, String FIGURE) {
+        this.NAME = NAME;
+        this.FIGURE = FIGURE;
+    }
+
+    public String getNAME() {
+        return NAME;
+    }
+
+    public String getFIGURE() {
+        return FIGURE;
+    }
+
+
+
+
+}
