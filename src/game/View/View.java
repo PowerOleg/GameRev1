09bd95140456e86ff3c815b199565839fa9ed373
oleg <geraskin.oleg@mail.ru>
2022-07-24 19:45:07 +0500

    A View rev1 was created

---
+++ src/game/View/View.java
@@ -0,0 +1,34 @@
+package game.View;
+
+import game.Models.Field;
+
+public class View {
+    private Field field;
+
+
+    public View(Field field) {
+        this.field = field;
+        System.out.println("The new game has just began");
+        System.out.println("---------------------------");
+    }
+    public void showField() {
+        for (int i = 0; i < field.getSIZE_FIELD(); i += 1) {
+            for (int j = 0; j < field.getSIZE_FIELD(); j += 1) {
+                if (j != 0) {
+                    System.out.print("|");
+                }
+                System.out.printf("[%s]", field.getCellField(i, j));
+
+            }
+            if (i != field.getSIZE_FIELD()-1) {
+                System.out.println("\n-----------");
+            }
+
+
+        }
+        System.out.println("\n");
+    }
+
+
+
+}
