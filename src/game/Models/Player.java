545ae656f80c887e70a229155bf6c8862c336acd
oleg <geraskin.oleg@mail.ru>
2022-07-28 22:42:46 +0500

    Player rev2.

--- src/game/Models/Player.java
+++ src/game/Models/Player.java
@@ -2,7 +2,11 @@ package game.Models;
 
 public class Player {
     private final String NAME;
-    private final String FIGURE;
+    /*private final*/ String FIGURE;                            //добавил дичь v1
+
+    public void setFIGURE(String FIGURE) {                      //добавил дичь v1
+        this.FIGURE = FIGURE;
+    }
 
     public Player(String NAME, String FIGURE) {
         this.NAME = NAME;
