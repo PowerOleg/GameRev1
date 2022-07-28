19e92f5723a7feb805bc110abf408a11116f866b
oleg <geraskin.oleg@mail.ru>
2022-07-28 22:43:07 +0500

    Field rev2.

--- src/game/Models/Field.java
+++ src/game/Models/Field.java
@@ -11,9 +11,48 @@ public class Field {
         this.SIZE_FIELD = SIZE_FIELD;
         this.MIN_NUMBER = 0;
         this.MAX_NUMBER = getSIZE_FIELD();
-/* ? */        stateField = new String[getSIZE_FIELD()][getSIZE_FIELD()];
-/* ? */         setStateField(DEFAULT_SYMBOL);
+            stateField = new String[getSIZE_FIELD()][getSIZE_FIELD()];   //создается поле т.е. создается массив
+            setStateField(DEFAULT_SYMBOL);                               //поле заполняется пустыми клеточками
     }
+
+
+
+
+
+
+
+
+        public void setStateField(String symbol) {
+        for (int i = 0; i < getSIZE_FIELD(); i += 1) {
+            for (int j = 0; j < getSIZE_FIELD(); j +=1) {
+                setCellField(i , j, symbol);
+            }
+        }
+    }
+        public void setCellField(int i, int j, String symbol) {
+        if (checkedCoordinate(i) && checkedCoordinate(j)) {
+            if (stateField[i][j] == DEFAULT_SYMBOL || stateField[i][j] == null) {
+                this.stateField[i][j] = symbol;                              //команда кладет значение в ячейку массива
+            } else {
+                System.out.println("The cell is full");
+            }
+        }
+    }
+    private boolean checkedCoordinate(int coordinate) {
+        if (coordinate >= getMIN_NUMBER() && coordinate <= getMAX_NUMBER()) {
+            return true;
+        } else {
+            System.out.println("Wrong coordinates");
+            return false;
+        }
+    }
+
+
+
+
+
+
+
 
     public int getSIZE_FIELD() {
         return SIZE_FIELD;
@@ -41,35 +80,10 @@ public class Field {
         } else {
             return null;                                            //нужно будет потом удалить return null
         }
-    }
-
-/* ? */    public void setStateField(String symbol) {
-        for (int i = 0; i < getSIZE_FIELD(); i += 1) {
-            for (int j = 0; j < getSIZE_FIELD(); j +=1) {
-                setCellField(i , j, symbol);
-
-            }
-
-        }
 
     }
 
-/* ? symbol */    public void setCellField(int i, int j, String symbol) {
-    if (checkedCoordinate(i) && checkedCoordinate(j)) {
-        if (stateField[i][j] == DEFAULT_SYMBOL || stateField[i][j] == null) {
-            this.stateField[i][j] = symbol;
-        } else {
-            System.out.println("The cell is full");
-        }
-    }
-}
-private boolean checkedCoordinate(int coordinate) {
-    if (coordinate >= getMIN_NUMBER() && coordinate <= getMAX_NUMBER()) {
-        return true;
-    } else {
-        System.out.println("Wrong coordinates");
-        return false;
-    }
-}
+
+
 
 }
